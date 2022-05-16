8ac65a5a154d956221c1a0c0dd2e01af68583531
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-05-16 10:19:25 +0200

    Initial commit
    Add base project structure

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  
+    <header>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                    <font-awesome-icon icon="fa-solid fa-clock" />
+
+                    <span>Open Hours: Mon - Sat - 9:00 - 18:00</span>
+
+                </div>
+                <div class="col"></div>
+            </div>
+        </div>
+
+        <font-awesome-icon icon="fa-solid fa-phone" />
+
+    </header>
+
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+    transpileDependencies: true,
+
+    css: {
+        loaderOptions: {
+            scss: {
+                additionalData: `
+                  @import '@/assets/scss/style.scss';
+                  @import '@/assets/scss/variables.scss';
+                `
+            }
+        }
+    }
+})
